90216e08a27a239602f2c994de75dc2cc2455695
Aryan <aupathak1234@gmail.com>
2025-01-23 15:24:13 +0530

    split sign up into its own page, added user roles

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hackathon_app_rubix/screens/sign_up_screen.dart';
-import 'package:sign_in_button/sign_in_button.dart';
 
 import '../models/user_model.dart';
 import '../providers/user_provider.dart';
 import 'home_screen.dart';
 
-class LoginScreen extends ConsumerWidget {
-  LoginScreen({super.key});
+class SignUpScreen extends ConsumerWidget {
+  SignUpScreen({super.key});
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+  TextEditingController();
+  final TextEditingController nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    String userType = 'user';
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Sign Up'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -31,6 +34,16 @@ class LoginScreen extends ConsumerWidget {
                   child: FlutterLogo(),
                 ),
                 const SizedBox(height: 20),
+                TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    hintText: 'Your name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
                 TextField(
                   controller: emailController,
                   decoration: const InputDecoration(
@@ -51,12 +64,55 @@ class LoginScreen extends ConsumerWidget {
                     ),
                   ),
                 ),
+                const SizedBox(height: 10),
+                TextField(
+                  obscureText: true,
+                  controller: confirmPasswordController,
+                  decoration: const InputDecoration(
+                    hintText: 'Confirm Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                DropdownMenu<String>(
+                  dropdownMenuEntries: [
+                    DropdownMenuEntry<String>(
+                      value: 'teacher',
+                      label: 'Teacher',
+                    ),
+                    DropdownMenuEntry<String>(
+                      value: 'student',
+                      label: 'Student',
+                    ),
+                    DropdownMenuEntry<String>(
+                      value: 'user',
+                      label: 'Regular user',
+                    ),
+                  ],
+                  onSelected: (value) {
+                    userType = value ?? 'user';
+                  },
+                ),
                 const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () async {
-                    await ref.read(authNotifierProvider.notifier).signInUser(
+                    if (passwordController.text !=
+                        confirmPasswordController.text) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Passwords do not match'),
+                        ),
+                      );
+                      return;
+                    }
+
+                    await ref.read(authNotifierProvider.notifier).registerUser(
                       emailController.text,
                       passwordController.text,
+                      nameController.text,
+                      userType,
                       context,
                     );
                     await ref
@@ -73,39 +129,7 @@ class LoginScreen extends ConsumerWidget {
                       );
                     }
                   },
-                  child: const Text('Sign in'),
-                ),
-                const SizedBox(height: 20),
-                SignInButton(
-                  Buttons.google,
-                  onPressed: () async {
-                    await ref
-                        .watch(authNotifierProvider.notifier)
-                        .signInWithGoogle(context);
-                    await ref
-                        .watch(authNotifierProvider.notifier)
-                        .fetchUserDetails();
-                    UserModel? user =
-                        ref.watch(userModelProvider.notifier).state;
-                    if (user != null) {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HomeScreen(),
-                        ),
-                      );
-                    }
-                  },
-                ),
-                const SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignUpScreen()),
-                    );
-                  },
-                  child: const Text("Don't have an account? Sign Up"),
+                  child: const Text('Sign up'),
                 ),
               ]),
         ),
